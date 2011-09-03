c0dfb355fd9ad379f3e794e77681c6bfe971a35c
Bill Parrott <bill@chimericdream.com>
2011-09-03 10:15:10 -0500

    first functioning piece; border radius for -moz browsers

---
+++ src/CssPrefixer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @author Bill
+ */
+class CssPrefixer {
+    private $_browsers = array(
+        'explorer'  => true,
+        'konqueror' => false,
+        'mozilla'   => true,
+        'opera'     => true,
+        'webkit'    => true,
+    );
+    
+    public function __construct()
+    {
+        
+    } //end __construct
+    
+    public function prefix()
+    {
+    } //end prefix
+
+    public function getBrowser($browser)
+    {
+    } //end getBrowser
+    
+    public function setBrowser($browser, $generate)
+    {
+    } //end setBrowser
+    
+    public function setBrowsers(array $browsers)
+    {
+    } //end setBrowsers
+    
+    public function borderRadius($cssString = '')
+    {
+        
+    } //end borderRadius
+} //end class CssPrefixer
