d7dc50b7f3b82b3c8d915c948236e30e748ece1b
Bill Parrott <bill@chimericdream.com>
2011-09-06 08:35:12 -0500

    added browser object includes

--- src/Browser.php
+++ src/Browser.php
@@ -1,4 +1,10 @@
 <?php
+require_once dirname(__FILE__) . '/Browser/Explorer.php';
+require_once dirname(__FILE__) . '/Browser/Konqueror.php';
+require_once dirname(__FILE__) . '/Browser/Mozilla.php';
+require_once dirname(__FILE__) . '/Browser/Opera.php';
+require_once dirname(__FILE__) . '/Browser/Webkit.php';
+
 /**
  * Description of Browser
  *
--- src/CssAlly.php
+++ src/CssAlly.php
@@ -1,4 +1,6 @@
 <?php
+require_once dirname(__FILE__) . '/Browser.php';
+
 /**
  * @author Bill
  */
